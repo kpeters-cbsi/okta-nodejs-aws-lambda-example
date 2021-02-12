0ace457c2333d6d2298e1a51506467032352e75d
Kit Peters <christopher.peters@cbsinteractive.com>
2021-02-12 10:08:12 -0600

    clean up formatting

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const session = require('express-session');
-const { ExpressOIDC } = require('@okta/oidc-middleware');
+const express = require('express')
+const session = require('express-session')
+const { ExpressOIDC } = require('@okta/oidc-middleware')
 
-const absPathPrefix = process.env.ABSOLUTE_PATH_PREFIX || '';
+const absPathPrefix = process.env.ABSOLUTE_PATH_PREFIX || ''
 
-const app = express();
+const app = express()
 
-app.use(session({
-  secret: process.env.APP_SECRET,
-  resave: true,
-  saveUninitialized: false,
-}));
+app.use(
+  session({
+    secret: process.env.APP_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+)
 
 const oidc = new ExpressOIDC({
   issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
@@ -23,24 +25,25 @@ const oidc = new ExpressOIDC({
   loginRedirectUri: `${process.env.HOST_URL}${absPathPrefix}/authorization-code/callback`,
   routes: {
     loginCallback: {
-      afterCallback: `${absPathPrefix}/where-is-it`
+      afterCallback: `${absPathPrefix}/where-is-it`,
     },
   },
-});
+})
 
-const ensureAuthenticated = oidc.ensureAuthenticated(`${absPathPrefix}/login`);
+const ensureAuthenticated = oidc.ensureAuthenticated(`${absPathPrefix}/login`)
 
-app.use(oidc.router);
+app.use(oidc.router)
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080
 
-app.get('/where-is-it', ensureAuthenticated, require('./where-is-it'));
-app.use('/', (req, res) => res.redirect(`${absPathPrefix}/where-is-it`));
+app.get('/where-is-it', ensureAuthenticated, require('./where-is-it'))
+app.use('/', (req, res) => res.redirect(`${absPathPrefix}/where-is-it`))
 
 if (process.env.LAMBDA_TASK_ROOT) {
-  const serverlessExpress = require('aws-serverless-express');
-  const server = serverlessExpress.createServer(app);
-  exports.handler = (event, context) => serverlessExpress.proxy(server, event, context);
+  const serverlessExpress = require('aws-serverless-express')
+  const server = serverlessExpress.createServer(app)
+  exports.handler = (event, context) =>
+    serverlessExpress.proxy(server, event, context)
 } else {
-  app.listen(port, () => console.log(`Listening on port ${port}`));
+  app.listen(port, () => console.log(`Listening on port ${port}`))
 }
