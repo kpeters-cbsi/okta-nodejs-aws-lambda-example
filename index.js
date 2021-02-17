b81dfb7c4cfaf91c51212d0b53eb7b7dfb853813
Kit Peters <christopher.peters@cbsinteractive.com>
2021-02-17 12:59:00 -0600

    Wait for OIDC 'ready' event as Promise
    It was suggested at https://github.com/okta/okta-oidc-js/issues/68
    that the "unknown authentication strategy 'oidc'" error could be
    due to a race condition, and that one should wait for the "ready"
    event from ExpressOIDC. This change converts the "ready" event into
    a Promise and updates the handler to wait for this promise before
    proxying the API Gatewway event.
    
    With this change, the "unknown authentication strategy" error goes
    away, but an unspecified internal error takes its place.

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
 const { ExpressOIDC } = require('@okta/oidc-middleware')
-
-const hostUrl = process.env.LAMBDA_TASK_ROOT ? process.env.HOST_URL_LAMBDA : process.env.HOST_URL_LOCAL
+const { fromEvent } = require('promise-toolbox')
+const hostUrl = process.env.LAMBDA_TASK_ROOT
+  ? process.env.HOST_URL_LAMBDA
+  : process.env.HOST_URL_LOCAL
 
 const absPathPrefix =
   (process.env.LAMBDA_TASK_ROOT ? process.env.ABSOLUTE_PATH_PREFIX : '') || ''
@@ -36,6 +38,8 @@ const oidc = new ExpressOIDC({
   },
 })
 
+const readyPromise = fromEvent(oidc, 'ready')
+
 const ensureAuthenticated = oidc.ensureAuthenticated(`${absPathPrefix}/login`)
 
 app.use(oidc.router)
@@ -43,13 +47,19 @@ app.use(oidc.router)
 const port = process.env.PORT || 8080
 
 app.get('/where-is-it', ensureAuthenticated, require('./where-is-it'))
-app.use('/', (req, res) => res.redirect(`${absPathPrefix}/where-is-it`))
+app.use('/', (_req, res) => res.redirect(`${absPathPrefix}/where-is-it`))
 
 if (process.env.LAMBDA_TASK_ROOT) {
   const serverlessExpress = require('aws-serverless-express')
   const server = serverlessExpress.createServer(app)
-  exports.handler = (event, context) =>
-    serverlessExpress.proxy(server, event, context)
+  exports.handler = async (event, context) => {
+    console.log('Waiting for OIDC to become ready')
+    const readyResult = await readyPromise
+    console.log('OIDC is ready ', { readyResult })
+    const ret = serverlessExpress.proxy(server, event, context)
+    console.log('Executed proxy: ', { ret })
+    return ret
+  }
 } else {
   app.listen(port, () => console.log(`Listening on port ${port}`))
 }
